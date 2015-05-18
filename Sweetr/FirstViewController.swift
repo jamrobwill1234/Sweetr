01043ea310e37dadab30a9c229938ca01e52de31
James W <jamrobwill1234@gmail.com>
2015-05-18 10:22:51 -0400

    App Create

---
+++ Sweetr/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Sweetr
+//
+//  Created by James Williams on 5/18/15.
+//  Copyright (c) 2015 James Williams. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
