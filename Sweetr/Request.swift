83680d77ddee61934c87ae286681c38814ce2bba
James W <jamrobwill1234@gmail.com>
2015-05-26 15:15:12 -0400

    New Commit

--- Sweetr/Request.swift
+++ Sweetr/Request.swift
@@ -12,18 +12,22 @@ import CoreLocation
 
 
 class FourSquareRequest {
- 
-   let API_URL = "https://api.foursquare.com/v2/"
-   let CLIENT_ID = "4FP4Y43MH4HNTJ2YS1HXONYQ2KYJLMPR0UZE1Y2C2ZLMD3Z5"
-   let CLIENT_SECRET = "ZDOAJYGNN5HG4T4SLZSKQJ1VEGNY1U5NAFTMTXURNA5LDWOQ"
     
-    func getVenuesWithLocation(location: CLLocation, completion: (venues: [AnyObject]) -> ())
-    {
-        
+    let API_URL = "https://api.foursquare.com/v2/"
+    let CLIENT_ID = "4FP4Y43MH4HNTJ2YS1HXONYQ2KYJLMPR0UZE1Y2C2ZLMD3Z5"
+    let CLIENT_SECRET = "ZDOAJYGNN5HG4T4SLZSKQJ1VEGNY1U5NAFTMTXURNA5LDWOQ"
     
-        
-        
-    }
+    
+    
+}
+
+func getVenuesWithLocation(location: CLLocation, completion: (venues: [AnyObject]) -> ())
+{
+    
+    
+    
+    
+    
     
 }
 
@@ -31,6 +35,129 @@ class FourSquareRequest {
 
 class TwitterRequest {
     
-    let API_URL = "https://api.twitter.com/1.1"
+    let API_URL = "https://api.twitter.com/1.1/"
+    let API_KEY = "SA5ZVnyWSe8KcphTFYjaDCCTu"
+    let API_SECRET = "JlWaWli1yKhHTJ6GdYgJDf9e9xKxxyA37WbOgoeZzLWqfRL7CF"
+    
+    var API_ENCODED: String? {
+        get {
+            
+            let str = API_KEY + ":" + API_SECRET
+            
+            let utf8str = str.dataUsingEncoding(NSUTF8StringEncoding)
+            
+            if let encoded = utf8str?.base64EncodedStringWithOptions(NSDataBase64EncodingOptions.allZeros){
+                
+                return "Basic " + encoded
+            }
+            return nil
+        }
+    }
+    
+    
+    
+    
+    
+    var TOKEN: String? {
+        
+        get {
+            
+            return NSUserDefaults.standardUserDefaults().objectForKey("twitterToken") as? String
+        }
+        
+        set {
+            
+            NSUserDefaults.standardUserDefaults().setObject(newValue, forKey: "twitterToken")
+            
+            NSUserDefaults.standardUserDefaults().synchronize()
+        }
+    }
+    func getToken(completion: () -> ()) {
+        let endpoint = "https://api.twitter.com/oauth2/token?grant_type=client_credentials"
+        if let url = NSURL(string: endpoint) {
+            var request = NSMutableURLRequest(URL: url)
+            
+            request.HTTPMethod = "POST"
+            
+            request.setValue(API_ENCODED, forHTTPHeaderField: "Authorization")
+            
+            request.setValue("application/x-www-form-urlencoded;charset=UTF-8.", forHTTPHeaderField: "Content-Type")
+            
+            NSURLConnection.sendAsynchronousRequest(request, queue: NSOperationQueue.mainQueue(), completionHandler: { (response, data, error) -> Void in
+                
+                var error: NSError? = nil
+                
+                if let returnInfo = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.MutableContainers, error: &error) as? [String:AnyObject] {
+                    
+                    println(returnInfo)
+                    
+                    
+                    self.TOKEN = returnInfo["access_token"] as? String
+                    
+                    completion()
+                    
+                }
+                
+                if let error = error {
+                    
+                    println(error)
+                }
+                
+            })
+            
+            
+        }
+        
+        
+    }
+    func findPopularFoodTweets(completion: (tweets: [AnyObject]) -> ()) {
+        
+        if TOKEN == nil {
+            
+            getToken({ () -> () in
+                
+                self.findPopularFoodTweets(completion)
+                
+            })
+            return // if there is no token we will not go any further
+        }
+        
+        let endpoint = API_URL + "search/tweets.json?q=Food&result_type=popular"
+        
+        
+        if let url = NSURL(string: endpoint) {
+            
+            let request = NSMutableURLRequest(URL: url)
+            
+            request.setValue("Bearer " + TOKEN!, forHTTPHeaderField: "Authorization")
+            // this is ok to force unwrap because the line above will prevent it from crashing by returning nil 
+            
+            NSURLConnection.sendAsynchronousRequest(request, queue: NSOperationQueue.mainQueue(), completionHandler: { (response, data, error) -> Void in
+                
+                var error: NSError? = nil
+                
+                if let searchInfo = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.MutableContainers, error: &error) as? [String: AnyObject] {
+                    
+                    if let tweets = searchInfo["statuses"] as? [AnyObject] {
+                        
+                        println(tweets)
+                        completion(tweets: tweets)
+
+                    }
+                    
+                   
+                                       
+                }
+                
+                if let error = error {
+                    
+                    println(error)
+                }
+                
+            })
+            
+        }
+    }
+    
     
 }
--- Sweetr/TweetsTableViewController.swift
+++ Sweetr/TweetsTableViewController.swift
@@ -10,10 +10,17 @@ import UIKit
 
 class TweetsTableViewController: UITableViewController {
 
+    var foundTweets: [AnyObject] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-    
+        TwitterRequest().findPopularFoodTweets { (tweets) -> () in
+            
+           self.foundTweets = tweets
+            self.tableView.reloadData()
+            
+        }
 
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
@@ -29,27 +36,27 @@ class TweetsTableViewController: UITableViewController {
 
     // MARK: - Table view data source
 
-    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        // #warning Potentially incomplete method implementation.
-        // Return the number of sections.
-        return 0
-    }
+//    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+//        // #warning Potentially incomplete method implementation.
+//        // Return the number of sections.
+//        return 0
+//    }
 
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete method implementation.
         // Return the number of rows in the section.
-        return 0
+        return foundTweets.count
     }
 
-    /*
+    
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath) as! UITableViewCell
+        let cell = tableView.dequeueReusableCellWithIdentifier("tweetsCell", forIndexPath: indexPath) as! UITableViewCell
 
-        // Configure the cell...
+        cell.textLabel?.text = foundTweets[indexPath.row]["text"] as? String
 
         return cell
     }
-    */
+    
 
     /*
     // Override to support conditional editing of the table view.
